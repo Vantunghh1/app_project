c27360a940f8092254191ff7b94f20e7cc10b64c
Vantunghh1 <vantunghh1@gmail.com>
2023-09-26 11:10:49 +0700

    add_giao_dien_pt_b2_m_bien git commit -madd_giao_dien_pt_b2_m_bien git commit -madd_giao_dien_pt_b2_m_bien

--- add_giao_dien_pt_b2.py
+++ add_giao_dien_pt_b2.py
@@ -2,14 +2,14 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 
-
 def giai_he_pttt():
     try:
+        m = int(nhap_m.get())
         n = int(nhap_n.get())
         phuong_trinh = []
         for i in range(n):
             pt = []
-            for j in range(n):
+            for j in range(m):
                 pt.append(float(nhap_bien[i][j].get()))
             phuong_trinh.append(pt)
 
@@ -23,16 +23,15 @@ def giai_he_pttt():
     except Exception as e:
         messagebox.showerror("Lỗi", str(e))
 
-
 def reset_fields():
     nhap_n.delete(0, tk.END)
+    nhap_m.delete(0, tk.END)
     ket_qua_text.delete(1.0, tk.END)
     for khung_phuong_trinh in cua_so.winfo_children():
         if isinstance(khung_phuong_trinh, tk.Frame):
             khung_phuong_trinh.destroy()
     nhap_bien.clear()
     nhap_hang_so.clear()
-
 
 def xoa_truong_phuong_trinh():
     for danh_sach_bien in nhap_bien:
@@ -41,7 +40,6 @@ def xoa_truong_phuong_trinh():
     for nhap_hang_so_var in nhap_hang_so:
         nhap_hang_so_var.delete(0, tk.END)
     ket_qua_text.delete(1.0, tk.END)
-
 
 def tao_cac_truong_phuong_trinh():
     try:
@@ -53,15 +51,14 @@ def tao_cac_truong_phuong_trinh():
         nhap_hang_so.clear()
 
         n = int(nhap_n.get())
-        nhap_bien.clear()
-        nhap_hang_so.clear()
+        m = int(nhap_m.get())
 
         for i in range(n):
             khung_phuong_trinh = tk.Frame(cua_so)
             khung_phuong_trinh.pack()
 
             danh_sach_bien = []
-            for j in range(n):
+            for j in range(m):
                 nhap_bien_var = tk.Entry(khung_phuong_trinh, width=5)
                 nhap_bien_var.pack(side=tk.LEFT)
                 danh_sach_bien.append(nhap_bien_var)
@@ -78,17 +75,21 @@ def tao_cac_truong_phuong_trinh():
     except Exception as e:
         messagebox.showerror("Lỗi", str(e))
 
-
 # Tạo cửa sổ giao diện
 cua_so = tk.Tk()
 cua_so.title("Giải Hệ Phương Trình Tuyến Tính NHÓM 5")
 cua_so.geometry("1000x1000")
 
-# Nhập số hệ phương trình
+# Nhập số hệ phương trình và số biến
 nhan_n = tk.Label(cua_so, text="Nhập số hệ phương trình (n):")
 nhan_n.pack()
 nhap_n = tk.Entry(cua_so)
 nhap_n.pack()
+
+nhan_m = tk.Label(cua_so, text="Nhập số biến (m):")
+nhan_m.pack()
+nhap_m = tk.Entry(cua_so)
+nhap_m.pack()
 
 # Tạo danh sách các biến và hằng số
 nhap_bien = []
