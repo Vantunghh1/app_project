15468be4472abed4744a5b0b5c83ea082c2ae511
Vantunghh1 <vantunghh1@gmail.com>
2023-09-29 11:04:24 +0700

    add giao dien thong ke demo

--- Bai1_thong_ke.py
+++ Bai1_thong_ke.py
@@ -1,23 +1,37 @@
+import tkinter as tk
 import pandas as pd
 from numpy import array
 import matplotlib.pyplot as plt
 import numpy as np
+from tkinter import messagebox
 
 df = pd.read_csv('diemPython.csv', index_col=0, header=0)
 in_data = array(df.iloc[:, :])
-print(in_data)
-print('Tong so sinh vien di thi :')
-tongsv = in_data[:, 1]
-print(np.sum(tongsv))
-diemA = in_data[:, 3]
-diemBc = in_data[:, 4]
-print('Tong sv:', tongsv)
-maxa = diemA.max()
-i, = np.where(diemA == maxa)
-print('lop co nhieu diem A la {0} co {1} sv dat diem A'.format(in_data[i, 0], maxa))
-plt.plot(range(len(diemA)), diemA, 'r-', label="Diem A")
-plt.plot(range(len(diemBc)), diemBc, 'g-', label="Diem B +")
-plt.xlabel('Lơp')
-plt.ylabel(' So sv dat diem ')
-plt.legend(loc='upper right')
-plt.show()
+def read_text_file():
+    # Tạo một cửa sổ mới để hiển thị dữ liệu
+    new_window = tk.Toplevel(window)
+    new_window.title('Dữ liệu từ tệp văn bản')
+    text_widget = tk.Text(new_window)
+    text_widget.insert(tk.END, in_data)
+    text_widget.pack()
+def tong_SV_di_thi():
+    new_window = tk.Toplevel(window)
+    new_window.title('Tong so sinh vien di thi :')
+    tongsv = in_data[:, 1]
+    Tong = np.sum(tongsv)
+    text_widget = tk.Text(new_window)
+    text_widget.insert(tk.END, tongsv)
+
+window = tk.Tk()
+window.title('Giao diện Python')
+window.geometry('600x600')
+
+button = tk.Button(window, text='Hiển thị dữ liệu', command=read_text_file)
+button.grid(column=1, row=0)
+
+
+button2 = tk.Button(window, text='Tổng sinh viên đi thi', command=tong_SV_di_thi)
+button2.grid(column=2, row=0)
+
+
+window.mainloop()
