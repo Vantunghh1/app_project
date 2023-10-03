ff5f5701537dbd116ad4d536ed3ec90e2ae08231
Vantunghh1 <vantunghh1@gmail.com>
2023-10-03 10:20:11 +0700

    hien thi lop co nhieu diem A nhat và vẽ bieu do diem A so voi diem B khi nhan nut

--- Bai1_thong_ke.py
+++ Bai1_thong_ke.py
@@ -22,6 +22,24 @@ def tong_SV_di_thi():
     label = tk.Label(new_window, text=f"Tổng số sinh viên tham gia thi: {Tong}")
     label.pack()
 
+def diem_A():
+    new_window = tk.Toplevel(window)
+    new_window.title('Du lieu ve sl diem A :')
+    diemA = in_data[:, 3]
+    diemBc = in_data[:, 4]
+    maxa = diemA.max()
+    indexes = np.where(diemA == maxa)
+    lop_nhieu_diem_A = [in_data[i, 0] for i in indexes]
+    label = tk.Label(new_window, text=f"lop co nhieu diem A la: {lop_nhieu_diem_A}")
+    label.pack()
+
+    plt.plot(range(len(diemA)), diemA, 'r-', label="Diem A")
+    plt.plot(range(len(diemBc)), diemBc, 'g-', label="Diem B +")
+    plt.xlabel('Lơp')
+    plt.ylabel(' So sv dat diem ')
+    plt.legend(loc='upper right')
+    plt.show()
+
 
 window = tk.Tk()
 window.title('Giao diện Python')
@@ -33,6 +51,9 @@ button.grid(column=1, row=0)
 
 button2 = tk.Button(window, text='Tổng sinh viên đi thi', command=tong_SV_di_thi)
 button2.grid(column=2, row=0)
+
+button3 = tk.Button(window, text='DL diem A', command=diem_A)
+button3.grid(column=3, row=0)
 
 
 window.mainloop()
